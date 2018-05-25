6ca5b64cb73e0d4ca2fca27fce98c6531201442c
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2018-05-25 14:34:44 +0200

    kart og barplot lagt til (#3)
    kart og barplot lagt til

--- inst/app/server.R
+++ inst/app/server.R
@@ -1,25 +1,43 @@
+library(shiny)
+library(ggplot2)
+library(leaflet)
+
 shinyServer(
 
   function(input, output) {
-    output$waiting <- renderPlot({
-
-      x    <- faithful$waiting
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-      hist(x, breaks = bins, col = "#75AADB", border = "white",
-           xlab = "Waiting time to next eruption (in mins)",
-           main = "Histogram of waiting times")
-
+    
+    
+    kartlagInput <- reactive({
+      switch(input$kartlag,
+             "Personer til fastlege/legevakt" = kols$FLLV_pers_Rate,
+             "Personer poliklinikk" = kols$Poli_Pers_Rate,
+             "Akuttinnlagte personer" = kols$Akutt_pers_Rate)
     })
     
-    output$eruptions <- renderPlot({
-      x <- faithful$eruptions
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    output$kolstabell<-renderTable(
+      kartlagInput()
+      )
+
+    output$kolshisto <- renderPlot({
       
-      hist(x, breaks = bins, col = "#75AADB", border = "white",
-           xlab = "Eruption time (in mins)",
-           main = "Histogram of eruption times")
+      kolsdata <- data.frame(bohf=kols$Opptaksomr, kartlag=kartlagInput())
+      
+      # barplot
+        ggplot(data=kolsdata, aes(x=reorder(bohf, kartlag), y=kartlag)) +
+        geom_bar(stat="identity", fill="#95BDE6") + 
+        labs(x = "Opptaksområde", y = input$kartlag) + 
+        theme(axis.text.x = element_text(angle = 45, hjust = 1)) + 
+        theme(panel.background = element_blank())
       
     })
     
-  })
+    output$mymap <- renderLeaflet({
+      leaflet() %>%
+        addTiles() %>%  # Add default OpenStreetMap map tiles
+        addMarkers(lng=174.768, lat=-36.852, popup="The birthplace of R")
+      
+    })
+    
+  }
+  
+)
--- inst/app/ui.R
+++ inst/app/ui.R
@@ -1,13 +1,27 @@
+library(shiny)
+library(leaflet)
+
 shinyUI(function(request){
   navbarPage(title = "",
-             tabPanel("input", sliderInput(inputId = "bins",
-                                           label = "Number of bins:",
-                                           min = 1,
-                                           max = 50,
-                                           value = 30)),
+             tabPanel("Velg tema", 
+                      sidebarPanel(selectInput(inputId = "kartlag",
+                                          label = "Velg et tema:",
+                                          choices = c("Personer til fastlege/legevakt", 
+                                                      "Personer poliklinikk", 
+                                                      "Akuttinnlagte personer"))
+                      ),
+                      mainPanel(tableOutput("kolstabell")
+                        
+                      )
+             ),
+             
+             
+             
              navbarMenu(title="Plots",
-                        tabPanel("Waiting", plotOutput(outputId = "waiting")),
-                        tabPanel("Eruptions", plotOutput(outputId = "eruptions"))
+                         tabPanel("kart", leafletOutput("mymap")),
+                         tabPanel("Kols histogram", plotOutput(outputId = "kolshisto"))
              )
+
+            
   )
 })
