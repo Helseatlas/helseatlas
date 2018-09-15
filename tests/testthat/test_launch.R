3dda3448a5c8a99b4e66566abbeb2c06445c707c
Arnfinn <arnfinn.steindal@uit.no>
2018-09-15 20:25:46 +0200

    minor

--- tests/testthat/test_launch.R
+++ tests/testthat/test_launch.R
@@ -2,5 +2,4 @@ context("Create tmpdir")
 
 test_that("The temporary directory can be created", {
   expect_is(create_appDir(healthatlas_data = NULL), "character")
-}
-)
+})
--- tests/testthat/test_plot_variation.R
+++ tests/testthat/test_plot_variation.R
@@ -4,4 +4,7 @@ test_that("plot_variation is working", {
   tmp <- readRDS("data/filter_1.rds")
   myplot <- plotVariation(inputData = tmp, xlab = "Opptaksomr", ylab = "Rate", type = "histogram")
   expect_equal_to_reference(myplot, "data/plot1.rds")
+  
+  myplot <- plotVariation(inputData = tmp, xlab = "Opptaksomr", ylab = "Rate", type = "qwerty")
+  expect_null(myplot)
 })
