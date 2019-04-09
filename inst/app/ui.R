27fb05183b41733487f3c43bdbefbb6186268b84
arnfinn <arnfinn.steindal@gmail.com>
2019-04-09 10:56:30 +0200

    polish

--- inst/app/ui.R
+++ inst/app/ui.R
@@ -1,11 +1,9 @@
-
 shinyUI(function(request){
   fluidPage(theme = shinythemes::shinytheme("cerulean"),
   titlePanel(tags$head(tags$link(rel = "icon", type = "image/png", href = "hn.png"),
                                   tags$title("Helseatlas"),
                                   tags$style(type = "text/css", "a{color: #808080;}"), # Link color, and inactive tab color
-                                  tags$style(type = "text/css", "h1{color: #003A8C;}")#, # Title color
-  #                                tags$style(type="text/css", ".container-fluid {  max-width: 1200px;}") # max page width
+                                  tags$style(type = "text/css", "h1{color: #003A8C;}") # Title color
    )
    ), uiOutput("title"),
   
@@ -15,7 +13,7 @@ shinyUI(function(request){
       uiOutput("pickLevel3")
     ),
     mainPanel(
-        tabsetPanel(#type = "tabs", id="tab",
+        tabsetPanel(
                  tabPanel(uiOutput("titleTable"), uiOutput("makeTable")),
                  tabPanel(uiOutput("titleMap"), uiOutput("pickMap"), uiOutput("plotMap")),
                  tabPanel(uiOutput("titleHist"), uiOutput("plotHistogram"))
