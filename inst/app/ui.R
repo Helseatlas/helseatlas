eabc75c4695d8946076383ccaef95b854aa07d5e
Are Edvardsen <biorakel@gmail.com>
2019-05-10 14:09:09 +0200

    less verbose... (#66)

--- inst/app/ui.R
+++ inst/app/ui.R
@@ -1,25 +1,26 @@
+library(shiny)
 
-shiny::shinyUI(function(request) {
-  shiny::fluidPage(
+shinyUI(function(request) {
+  fluidPage(
     theme = shinythemes::shinytheme("cerulean"),
-    shiny::titlePanel(tags$head(
+    titlePanel(tags$head(
       tags$link(rel = "icon", type = "image/png", href = "hn.png"),
       tags$title("Helseatlas"),
       tags$style(type = "text/css", "a{color: #808080;}"), # Link and inactive tab color
       tags$style(type = "text/css", "h1{color: #003A8C;}") # , # Title color
-    )), shiny::uiOutput("title"),
+    )), uiOutput("title"),
 
-    shiny::sidebarPanel(
-      shiny::uiOutput("pick_level1"),
-      shiny::uiOutput("pick_level2"),
-      shiny::uiOutput("pick_level3")
+    sidebarPanel(
+      uiOutput("pick_level1"),
+      uiOutput("pick_level2"),
+      uiOutput("pick_level3")
     ),
-    shiny::mainPanel(
-      shiny::tabsetPanel(
-        shiny::tabPanel(shiny::uiOutput("title_overview"), shiny::uiOutput("make_overview")),
-        shiny::tabPanel(shiny::uiOutput("title_table"), shiny::uiOutput("make_table")),
-        shiny::tabPanel(shiny::uiOutput("title_map"), shiny::uiOutput("pick_map"), shiny::uiOutput("plot_map")),
-        shiny::tabPanel(shiny::uiOutput("title_hist"), shiny::uiOutput("plot_histogram"))
+    mainPanel(
+      tabsetPanel(
+        tabPanel(uiOutput("title_overview"), uiOutput("make_overview")),
+        tabPanel(uiOutput("title_table"), uiOutput("make_table")),
+        tabPanel(uiOutput("title_map"), uiOutput("pick_map"), uiOutput("plot_map")),
+        tabPanel(uiOutput("title_hist"), uiOutput("plot_histogram"))
       )
     )
   )
