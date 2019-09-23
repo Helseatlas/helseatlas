dcc9d9a309dc507bbd14e432598b3b498b35fe8c
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2019-09-23 15:01:57 +0200

    Do not use proxy in launch_app (#101)
    Better do it outside, if needed

--- R/run_app.r
+++ R/run_app.r
@@ -7,7 +7,6 @@
 #' @param publish_app If TRUE: deploy app to shinyapps.io (default = FALSE)
 #' @param name The appName of the deployed shiny application (default = "experimental")
 #' @param shiny_account Which shiny account on shinyapps.io (default = "skde")
-#' @param proxy_url If url is given: deploy app through proxy (default = FALSE)
 #'
 #' @export
 launch_app <- function(dataset = NULL,
@@ -16,8 +15,7 @@ launch_app <- function(dataset = NULL,
                        title = NULL,
                        publish_app = FALSE,
                        name = "experimental",
-                       shiny_account = "skde",
-                       proxy_url = FALSE) {
+                       shiny_account = "skde") {
 
   # Create a directory with all necessary data.
   shinydir <- create_appdir(healthatlas_data = dataset,
@@ -27,10 +25,6 @@ launch_app <- function(dataset = NULL,
 
   # Run the app
   if (publish_app) {
-    if (proxy_url) {
-      options(RCurlOptions = list(proxy = proxy_url))
-      options(shinyapps.http = "rcurl")
-    }
     rsconnect::deployApp(appDir = shinydir, appName = name, account = shiny_account)
   } else {
     shiny::runApp(appDir = shinydir)
