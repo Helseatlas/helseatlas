e1ca09b667af20368f7026d96f4f172767644473
arnfinn <arnfinn.steindal@gmail.com>
2019-09-30 14:06:04 +0200

    English data of fodsel

--- tests/debugging/development.R
+++ tests/debugging/development.R
@@ -4,7 +4,7 @@ rm(list=ls())
 
 # All atlas data and maps
 all_data <- list(
-  "fodsel"   = list("data_no" = data::fodsel,   "data_en" = data::fodsel,     "year" = "2015–2017", "title_no" = "Fødselshjelp", "title_en" = "Obstetrics",  "map" = kart::fodsel),
+  "fodsel"   = list("data_no" = data::fodsel,   "data_en" = data::fodsel_en,  "year" = "2015–2017", "title_no" = "Fødselshjelp", "title_en" = "Obstetrics",  "map" = kart::fodsel),
   "gyn"      = list("data_no" = data::gyn,      "data_en" = data::gyn_en,     "year" = "2015–2017", "title_no" = "Gynekologi",   "title_en" = "Gynaecology", "map" = kart::gyn),
   "ortopedi" = list("data_no" = data::ortopedi, "data_en" = data::ortopedi,   "year" = "2012–2016", "title_no" = "Ortopedi",     "title_en" = "Orthopaedic", "map" = kart::gyn),
   "dagkir2"  = list("data_no" = data::dagkir2,  "data_en" = data::dagkir2_en, "year" = "2013–2017", "title_no" = "Dagkirurgi",   "title_en" = "Day surgey",  "map" = kart::dagkir2),
@@ -12,7 +12,8 @@ all_data <- list(
   "eldre"    = list("data_no" = data::kols,     "data_en" = data::kols_en,    "year" = "2013–2015", "title_no" = "Eldre",        "title_en" = "Elderly",     "map" = kart::kols),
   "nyfodt"   = list("data_no" = data::nyfodt,   "data_en" = data::nyfodt_en,  "year" = "2009–2014", "title_no" = "Nyfødt",       "title_en" = "Neonatal",    "map" = kart::nyfodt),
   "barn"     = list("data_no" = data::barn,     "data_en" = data::barn_en,    "year" = "2011–2014", "title_no" = "Barn",         "title_en" = "Children",    "map" = kart::barn),
-  "dagir"    = list("data_no" = data::dagkir,   "data_en" = data::dagkir_en,  "year" = "2011–2013", "title_no" = "Dagkirurgi",   "title_en" = "Day surgey",  "map" = kart::dagkir))
+  "dagir"    = list("data_no" = data::dagkir,   "data_en" = data::dagkir_en,  "year" = "2011–2013", "title_no" = "Dagkirurgi",   "title_en" = "Day surgey",  "map" = kart::dagkir)
+)
 
 # How to run the app through launch_app
 devtools::install_github("Helseatlas/kart")
@@ -33,6 +34,8 @@ shinymap::launch_app(
 )
 
 # How to run the app locally
+
+healthatlas_data <- all_data
 
 healthatlas_data <- shinymap::kols # or another dataset to use
 healthatlas_map <- geojsonio::geojson_read("tests/testthat/data/maps/test.geojson", what = "sp")
