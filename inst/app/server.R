1c5733125c0aad79603b406641c069860733626b
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2019-09-30 14:31:05 +0200

    selectize = false for all selectInput (#108)
    selectize = TRUE does not work well on phones. Opens keyboard.

--- inst/app/server.R
+++ inst/app/server.R
@@ -50,6 +50,7 @@ shiny::shinyServer(
         names(atlasnames) <- mytitle
 
         shiny::selectInput(
+          selectize = FALSE,
           inputId = "atlas",
           label = c("Velg atlas:", "Pick an atlas")[as.numeric(input$language)],
           choices = atlasnames,
@@ -88,6 +89,7 @@ shiny::shinyServer(
       pickable_level1 <- unique(factor(atlas_data()$level1_name))
       # The selector
       shiny::selectInput(
+        selectize = FALSE,
         inputId = "menu_level1",
         label = c("Velg et tema:", "Pick a subject")[as.numeric(input$language)],
         choices = pickable_level1,
@@ -106,6 +108,7 @@ shiny::shinyServer(
         pickable_level2 <- unique(factor(tmpdata$level2_name))
         # The selector
         shiny::selectInput(
+          selectize = FALSE,
           inputId = "menu_level2",
           label = c("Velg et tema:", "Pick a subject")[as.numeric(input$language)],
           choices = pickable_level2,
@@ -127,6 +130,7 @@ shiny::shinyServer(
         pickable_level3 <- unique(factor(tmpdata2$level3_name))
         # The selector
         shiny::selectInput(
+          selectize = FALSE,
           inputId = "menu_level3",
           label = c("Velg et tema:", "Pick a subject")[as.numeric(input$language)],
           choices = pickable_level3,
