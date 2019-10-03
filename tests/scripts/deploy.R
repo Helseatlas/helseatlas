1c2069fc69f823d175040dc643353c2b98600e42
arnfinn <arnfinn.steindal@gmail.com>
2019-10-03 18:22:10 +0200

    travis: setAccountInfo lib

--- tests/scripts/deploy.R
+++ tests/scripts/deploy.R
@@ -62,7 +62,7 @@ all_data <- list(
                     "map" = kart::dagkir)
 )
 
-setAccountInfo(name   = Sys.getenv("shinyapps_name"),
+rsconnect::setAccountInfo(name   = Sys.getenv("shinyapps_name"),
                token  = Sys.getenv("shinyapps_token"),
                secret = Sys.getenv("shinyapps_secret"))
 
