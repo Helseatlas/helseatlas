5b752460f5e6865d9952886ed4f6edf97c06a729
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2019-10-28 13:47:40 +0100

    Use SKDEr version of create_appdir (#121)

--- R/run_app.r
+++ R/run_app.r
@@ -1,7 +1,6 @@
 #' Launch the application, either locally or to shinyapps.io
 #'
 #' @param dataset The data set to be loaded into the application
-#' @param map The map to be used in the application
 #' @param title The title of the atlas
 #' @param publish_app If TRUE: deploy app to shinyapps.io (default = FALSE)
 #' @param name The appName of the deployed shiny application (default = "experimental")
@@ -11,7 +10,6 @@
 #'
 #' @export
 launch_app <- function(dataset = NULL,
-                       map = NULL,
                        title = NULL,
                        publish_app = FALSE,
                        name = "experimental",
@@ -20,11 +18,11 @@ launch_app <- function(dataset = NULL,
                        github_repo = NULL) {
 
   # Create a directory with all necessary data.
-  shinydir <- create_appdir(healthatlas_data = dataset,
-                            healthatlas_map = map,
-                            webpage_title = title,
-                            git_hash = git_hash,
-                            github_repo = github_repo)
+  shinydir <- SKDEr::create_appdir(app_data = dataset,
+                                   webpage_title = title,
+                                   package = "shinymap",
+                                   git_hash = git_hash,
+                                   github_repo = github_repo)
 
   # Run the app
   if (publish_app) {
@@ -32,44 +30,4 @@ launch_app <- function(dataset = NULL,
   } else {
     shiny::runApp(appDir = shinydir)
   }
-}
-
-#' Create an appDir for shiny::runApp and rsconnect::deployApp
-#'
-#' Create a directory in tempdir() where the installed version of
-#' shinymap package is copied and the data is saved.
-#' This directory, with its content, will be deployd to or ran by shiny.
-#'
-#' @param healthatlas_data The data to be saved in the directory, to be used by the app
-#' @param healthatlas_map The map
-#' @param webpage_title The title of the atlas
-#' @param git_hash Current git sha1 hash
-#' @param github_repo Current github repository
-#'
-#' @return The created directory
-#'
-create_appdir <- function(healthatlas_data = NULL,
-                          healthatlas_map = NULL,
-                          webpage_title = NULL,
-                          git_hash = NULL,
-                          github_repo = NULL) {
-  # Name the directory
-  tmpshinydir <- paste0(tempdir(), "/", "shiny")
-  # Delete old content in directory
-  unlink(tmpshinydir, recursive = TRUE, force = TRUE)
-  # Create main directory
-  dir.create(tmpshinydir)
-  # Copy the installed version of the shinymap package to the directory
-  file.copy(system.file("app", package = "shinymap"), tmpshinydir, recursive = TRUE)
-  # Create data folder
-  dir.create(paste0(tmpshinydir, "/", "app/data"))
-  # Save the data to a .RData file
-  save(healthatlas_data,
-       healthatlas_map,
-       webpage_title,
-       git_hash,
-       github_repo,
-       file = paste0(tmpshinydir, "/", "app/data/data.RData"))
-  # Return the name of the main directory
-  return(paste0(tmpshinydir, "/", "app"))
 }
--- tests/testthat/test_launch.R
+++
@@ -1,5 +0,0 @@
-context("Create tmpdir")
-
-test_that("The temporary directory can be created", {
-  expect_is(create_appdir(healthatlas_data = NULL), "character")
-})
