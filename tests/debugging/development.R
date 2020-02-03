5d5e0a7f980e89fc42246dcc15142b9461cbfc9a
arnfinn <arnfinn.steindal@gmail.com>
2020-02-03 13:11:22 +0100

    How to submit page: use ortopedic map

--- tests/debugging/development.R
+++ tests/debugging/development.R
@@ -10,7 +10,7 @@ devtools::install_github("Helseatlas/data")
 all_data <- list(
   "fodsel"   = list("data_no" = data::fodsel,   "data_en" = data::fodsel_en,  "year" = "2015–2017", "title_no" = "Fødselshjelp", "title_en" = "Obstetrics",  "map" = kart::fodsel),
   "gyn"      = list("data_no" = data::gyn,      "data_en" = data::gyn_en,     "year" = "2015–2017", "title_no" = "Gynekologi",   "title_en" = "Gynaecology", "map" = kart::gyn),
-  "ortopedi" = list("data_no" = data::ortopedi, "data_en" = data::ortopedi,   "year" = "2012–2016", "title_no" = "Ortopedi",     "title_en" = "Orthopaedic", "map" = kart::gyn),
+  "ortopedi" = list("data_no" = data::ortopedi, "data_en" = data::ortopedi,   "year" = "2012–2016", "title_no" = "Ortopedi",     "title_en" = "Orthopaedic", "map" = kart::ortopedi),
   "dagkir2"  = list("data_no" = data::dagkir2,  "data_en" = data::dagkir2_en, "year" = "2013–2017", "title_no" = "Dagkirurgi",   "title_en" = "Day surgey",  "map" = kart::dagkir2),
   "kols"     = list("data_no" = data::kols,     "data_en" = data::kols_en,    "year" = "2013–2015", "title_no" = "Kols",         "title_en" = "COPD",        "map" = kart::kols),
   "eldre"    = list("data_no" = data::eldre,    "data_en" = data::eldre_en,   "year" = "2013–2015", "title_no" = "Eldre",        "title_en" = "Elderly",     "map" = kart::eldre),
@@ -40,7 +40,8 @@ devtools::install_github("Helseatlas/shinymap")
 # Launch app
 shinymap::launch_app(
   dataset = all_data,
-  publish_app = TRUE
+  publish_app = TRUE,
+  name = "helseatlas"
 )
 
 # How to run the app locally
